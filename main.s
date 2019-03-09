6e68b6659a95a5c134c5601538a84f49b3dab75e
Kai0522 <kklin0522@gmail.com>
2019-03-09 14:37:24 +0800

    My Answer

--- main.s
+++ main.s
@@ -6,7 +6,24 @@
 .global _start
 .type _start, %function
 _start:
-	nop
+	mov    r0,#001
+    mov    r1,#002
+    mov    r2,#003
+    
+    push   {r0,r1,r2} //pushing forward
+    pop    {r3,r4,r5} //poping forward
+
+    mov    r3,0
+    mov    r4,0
+    mov    r5,0
+
+    push   {r0,r1,r2}
+    pop    {r3}       //testing pop 
+    pop    {r4}
+    pop    {r5}
+
+    push   {r2,r1,r0} //pushing backward
+    pop    {r5,r4,r3} //poping backward
 
 	//
 	//branch w/o link
